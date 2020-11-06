ef8223389bacaa90bc4cde84437322d6fa8bc422
Vatsal Raina <vatsalraina@hotmail.co.uk>
2020-11-06 19:49:27 +0000

    Built model for combined question-answering with in-built verification

--- combo_bert/models.py
+++ combo_bert/models.py
@@ -11,8 +11,8 @@ class BertQA(torch.nn.Module):
 
         self.bert = BertModel.from_pretrained("bert-base-uncased")
         self.qa_outputs = torch.nn.Linear(self.bert.config.hidden_size, 2)
-        self.verification_output = torch.nn.Linear(self.bert.config.hidden_size, 1)
-
+        self.classifier = torch.nn.Linear(self.bert.config.hidden_size, 1)
+        self.dropout = torch.nn.Dropout(self.bert.config.hidden_dropout_prob)
         # self.init_weights()
 
     
@@ -21,13 +21,14 @@ class BertQA(torch.nn.Module):
         outputs = self.bert(input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
 
         sequence_output = outputs[0]
+        pooled_output = outputs[1]
+        pooled_output = self.dropout(pooled_output)
 
         logits = self.qa_outputs(sequence_output)
         start_logits, end_logits = logits.split(1, dim=-1)
         start_logits = start_logits.squeeze(-1)
         end_logits = end_logits.squeeze(-1)
 
-        verification_logits = self.verification_output(sequence_output)
-        verification_logits = verification_logits.squeeze()
+        verification_logits = self.classifier(pooled_output)
 
         return start_logits, end_logits, verification_logits

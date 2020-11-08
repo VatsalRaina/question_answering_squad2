338695e64d9941870b37e83245ac243f378baf5e
Vatsal Raina <vatsalraina@hotmail.co.uk>
2020-11-08 16:06:59 +0000

    Test script for combined question-answering

--- combo_bert/test_qa.py
+++ combo_bert/test_qa.py
@@ -77,8 +77,8 @@ def main(args):
     too_long = 0
     for ex in dev_data:
         count+=1
-        if count==18:
-           break
+        # if count==18:
+        #    break
         question, passage = ex["question"], ex["context"]
         combo = question + " [SEP] " + passage
         inp_ids = tokenizer.encode(combo)

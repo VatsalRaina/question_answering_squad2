7357200502262faaa76cdc744208291723061ad6
Vatsal Raina <vatsalraina@hotmail.co.uk>
2020-11-24 10:16:14 +0000

    saliency

--- combo_electra/models.py
+++ combo_electra/models.py
@@ -2,7 +2,7 @@
 
 import torch
 import torchvision.models as models
-from transformers import ElectraModel, ElectraConfig
+from transformers import ElectraModel, ElectraConfig, ElectraClassificationHead
 
 class ElectraQA(torch.nn.Module):
     def __init__(self):
@@ -11,7 +11,7 @@ class ElectraQA(torch.nn.Module):
 
         self.electra = ElectraModel.from_pretrained("google/electra-base-discriminator")
         self.qa_outputs = torch.nn.Linear(self.electra.config.hidden_size, 2)
-        self.classifier = torch.nn.Linear(self.electra.config.hidden_size, 1)
+        self.classifier = ElectraClassificationHead.from_pretrained("google/electra-base-discriminator")
         self.dropout = torch.nn.Dropout(self.electra.config.hidden_dropout_prob)
         # self.init_weights()
 
@@ -21,15 +21,15 @@ class ElectraQA(torch.nn.Module):
         outputs = self.electra(input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
 
         sequence_output = outputs[0]
-        pooled_output = outputs[1]
-        pooled_output = self.dropout(pooled_output)
+        # pooled_output = outputs[1]
+        # pooled_output = self.dropout(pooled_output)
 
         logits = self.qa_outputs(sequence_output)
         start_logits, end_logits = logits.split(1, dim=-1)
         start_logits = start_logits.squeeze(-1)
         end_logits = end_logits.squeeze(-1)
 
-        verification_logits = torch.sigmoid(self.classifier(pooled_output))
+        verification_logits = torch.sigmoid(self.classifier(sequence_output))
 
         return start_logits, end_logits, verification_logits
     
@@ -38,14 +38,14 @@ class ElectraQA(torch.nn.Module):
         outputs = self.electra(inputs_embeds=input_embeds)
 
         sequence_output = outputs[0]
-        pooled_output = outputs[1]
-        pooled_output = self.dropout(pooled_output)
+        # pooled_output = outputs[1]
+        # pooled_output = self.dropout(pooled_output)
 
         logits = self.qa_outputs(sequence_output)
         start_logits, end_logits = logits.split(1, dim=-1)
         start_logits = start_logits.squeeze(-1)
         end_logits = end_logits.squeeze(-1)
 
-        verification_logits = torch.sigmoid(self.classifier(pooled_output))
+        verification_logits = torch.sigmoid(self.classifier(sequence_output))
 
         return start_logits, end_logits, verification_logits

0370f7dbcc04557f99086b20cbd71f798e8e94e0
Vatsal Raina <vatsalraina@hotmail.co.uk>
2020-12-11 16:42:47 +0000

    electra large

--- combo_electra/test_large.py
+++ combo_electra/test_large.py
@@ -70,8 +70,8 @@ def main(args):
     count = 0
     for ex in dev_data:
         count+=1
-        if count==18:
-           break
+        # if count==18:
+        #    break
         question, passage = ex["question"], ex["context"]
         inputs = tokenizer.encode_plus(question, passage, add_special_tokens=True, return_tensors="pt")
         inp_ids = inputs["input_ids"].tolist()[0]

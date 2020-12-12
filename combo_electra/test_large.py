bf9720d74db40f9ad5ebe94dd40af413b2088770
Vatsal Raina <vatsalraina@hotmail.co.uk>
2020-12-12 00:26:16 +0000

    electra large

--- combo_electra/test_large.py
+++ combo_electra/test_large.py
@@ -65,54 +65,52 @@ def main(args):
     print(len(dev_data))
 
     tokenizer = AutoTokenizer.from_pretrained("ahotrod/electra_large_discriminator_squad2_512")
-
-    input_ids = []
+    model = AutoModelForQuestionAnswering.from_pretrained("ahotrod/electra_large_discriminator_squad2_512")
     count = 0
     for ex in dev_data:
         count+=1
-        # if count==18:
-        #    break
+        if count==10:
+           break
         question, passage = ex["question"], ex["context"]
         inputs = tokenizer.encode_plus(question, passage, add_special_tokens=True, return_tensors="pt")
         inp_ids = inputs["input_ids"].tolist()[0]
-        if len(inp_ids) > 512:
-            inp_ids = inp_ids[:512]
-        input_ids.append(inp_ids)
-    # Pad all sequences with 0
-    max_len = max([len(sen) for sen in input_ids])
-    input_ids = pad_sequences(input_ids, maxlen=max_len, dtype="long", value=0, truncating="post", padding="post")
-    # Convert to torch tensors
-    input_ids = torch.tensor(input_ids)
-    input_ids = input_ids.long().to(device)
-    # Create the DataLoader for test set.
-    ds = TensorDataset(input_ids)
-    dl = DataLoader(ds, batch_size=args.batch_size, shuffle=False)
+ 
+        start_logits, end_logits = model(**inputs)
+        answer_start = torch.argmax(start_logits)
+        answer_end = torch.argmax(end_logits)
+        answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(inp_ids[answer_start:answer_end+1]))
 
-    model = AutoModelForQuestionAnswering.from_pretrained("ahotrod/electra_large_discriminator_squad2_512")
-    model.eval().to(device)
-    pred_start_logits = []
-    pred_end_logits = []
-    count = 0
-    for item in dl:
-        print(count)
-        count+=1
-        inp_id = item[0].to(device)
-        with torch.no_grad():
-            start_logits, end_logits = model(inp_id)
-        b_start_logits = start_logits.detach().cpu().numpy().tolist()
-        # pred_start_logits += b_start_logits
-        b_end_logits = end_logits.detach().cpu().numpy().tolist()
-        if len(pred_start_logits)==0:
-            pred_start_logits += b_start_logits
-            pred_end_logits += b_end_logits
-        else:
-            pred_start_logits.extend(b_start_logits)
-            pred_end_logits.extend(b_end_logits)
-        # pred_end_logits += b_end_logits
-    pred_start_logits, pred_end_logits = np.asarray(pred_start_logits), np.asarray(pred_end_logits)
+        if len(ex["answers"]["text"])!=0:
+            print(question)
+            print(passage)
+            print(ex["answers"]["text"])
+            print(answer)
+
+
+
+    # pred_start_logits = []
+    # pred_end_logits = []
+    # count = 0
+    # for item in dl:
+    #     print(count)
+    #     count+=1
+    #     inp_id = item[0].to(device)
+    #     with torch.no_grad():
+    #         start_logits, end_logits = model(inp_id)
+    #     b_start_logits = start_logits.detach().cpu().numpy().tolist()
+    #     # pred_start_logits += b_start_logits
+    #     b_end_logits = end_logits.detach().cpu().numpy().tolist()
+    #     if len(pred_start_logits)==0:
+    #         pred_start_logits += b_start_logits
+    #         pred_end_logits += b_end_logits
+    #     else:
+    #         pred_start_logits.extend(b_start_logits)
+    #         pred_end_logits.extend(b_end_logits)
+    #     # pred_end_logits += b_end_logits
+    # pred_start_logits, pred_end_logits = np.asarray(pred_start_logits), np.asarray(pred_end_logits)
     # Save all necessary file (in order to be able to ensemble)
-    np.savetxt(args.predictions_save_path + "pred_start_logits_all.txt", pred_start_logits)
-    np.savetxt(args.predictions_save_path + "pred_end_logits_all.txt", pred_end_logits)
+    # np.savetxt(args.predictions_save_path + "pred_start_logits_all.txt", pred_start_logits)
+    # np.savetxt(args.predictions_save_path + "pred_end_logits_all.txt", pred_end_logits)
 
 
 if __name__ == '__main__':
